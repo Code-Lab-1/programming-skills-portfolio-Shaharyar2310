6d6c8fdb5036cc2424494e1b7e27547cb0c1eebb
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2022-10-31 16:23:14 +0400

    the change

--- Chapter 3- Data Structures/Exercises/exercise 7.py
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -1,15 +1,15 @@
 ##exercise No. 7
 places = ["Machu Picchu", "Peru","The Grand Canyon","Colosseum,Amalfi","Angkor Wat","Pyramids of Giza"]
 print("original :" ,places)
-A = sorted(places)
+A = sorted(places)#sorted acending aplhabatical order
 print(A)
 print("original :" ,places)
-B = sorted(A, reverse=True)
+B = sorted(A, reverse=True)#sorted decending alphabatical order
 print(B)
-C = sorted(B, reverse=False)
+C = sorted(B, reverse=False)#sorted decending alphabatical reverse order
 print(C)
 print("original :" ,places)
-places.sort(reverse=False)
+places.sort(reverse=False)#acending alphabitical order
 print(places)
-places.sort(reverse=True)
+places.sort(reverse=True)#decending aplhabatical order
 print(places)
