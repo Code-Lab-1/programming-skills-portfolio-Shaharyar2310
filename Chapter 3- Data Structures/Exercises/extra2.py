bbb935cd53cc3b65ae92cfd9801fc9ebb0f26dab
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2022-11-23 13:54:20 +0400

    EXtra

---
+++ Chapter 3- Data Structures/Exercises/extra2.py
@@ -0,0 +1,5 @@
+#extra 2
+list=["milk","tomato","tooth picks","helmet","sponge","tv","cup","brocolli","towel","keys"]
+print(list)
+A = sorted(list)#acending aplhabatical order
+print(A)
---
+++ Chapter 3- Data Structures/Exercises/extra3.py
@@ -0,0 +1,7 @@
+#extra 3
+list1 =["milk","tomato","tooth picks","helmet"]
+list2 =["sponge","tv","cup","brocolli","towel","keys"]
+print(list1)
+print(list2)
+list3 =list1 + list2
+print(list3)
