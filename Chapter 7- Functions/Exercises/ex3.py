1292c9eb7f1a8cee723f07fe197368023602c6ce
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2022-11-23 09:56:08 +0400

    Chapter 7

---
+++ Chapter 7- Functions/Exercises/ex3.py
@@ -0,0 +1,7 @@
+#exercise 3 better
+def make_shirt():
+  size=input("enter your size here : ")
+  message=input("enter message here : ")
+  print("your size is",size, "and text on your shirt is" ,message)
+
+make_shirt()
---
+++ Chapter 7- Functions/Exercises/ex4.py
@@ -0,0 +1,17 @@
+#ex 4 
+def make_shirts():
+  size= 'L'
+  msg= 'i love python' 
+  print("your size is",size, "and text on your shirt is" ,msg)
+
+  size= 'M'
+  msg= 'i love python' 
+  print("your size is",size, "and text on your shirt is" ,msg)
+
+  size=input("enter your size here : ")
+  message=input("enter message here : ")
+  print("your size is",size, "and text on your shirt is" ,message)
+
+
+
+make_shirts()
